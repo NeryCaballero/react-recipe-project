05273e9f1726d62ce31573ea5b70170c73bd7232
NeryCaballero <nerycaballero24@gmail.com>
2021-07-26 23:52:36 +0200

    Added functionality for Add Recipe btn and Delete btn. Added local storage functionality.

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React,  { useState, useEffect } from 'react'
 import RecipeList from './RecipeList'
 import '../css/app.css'
+import { v4 as uuidv4 } from 'uuid';
+// import Recipe from './Recipe';
 
+export const RecipeContext = React.createContext()
+const LOCAL_STORAGE_KEY = 'reactRecipeProject.recipes'
 
 function App() {
+  const [recipes, setRecipes] = useState(sampleRecipes)
+
+  useEffect(() => {
+    const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (recipeJSON !== null) setRecipes(JSON.parse(recipeJSON))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes))
+  }, [recipes])
+
+
+  const recipeContextValue = {
+    handleRecipeAdd,
+    handleRecipeDelete
+  }
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instructions',
+      ingredients: [
+        {id: uuidv4(), name: 'Name', amount: '1 tbs.'}
+      ]
+    }
+
+    setRecipes([...recipes, newRecipe])
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter(recipe => recipe.id !== id))
+  }
+
   return (
-    <RecipeList recipes={sampleRecipes} />
+    <RecipeContext.Provider value={recipeContextValue}>
+      <RecipeList recipes={recipes} />
+    </RecipeContext.Provider>
+    
   )
 }
 
